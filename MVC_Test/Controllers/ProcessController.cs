39f0eb969cbda3259c540bfb1934d72f4301ea17
Kira1006 <ershilpa.tiwari.0001@gmail.com>
2017-05-24 16:50:59 +0530

    adding mvc test solution

---
+++ MVC_Test/Controllers/ProcessController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Test.Controllers
+{
+    public class ProcessController : Controller
+    {
+        //
+        // GET: /Process/
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string List()
+        {
+            return "Process controller calling";
+        }
+	}
+}
